3889381070789b1db21c09b97a8a131d4fa3ea24
gravcat <nick@adrift.io>
2016-09-08 00:11:58 -0500

    Set proper hosts in the members lists, instead of the placeholders

--- init-replica-set.js
+++ init-replica-set.js
@@ -3,9 +3,9 @@ rs.initiate(
 		_id: "rs-savino",
 		version: 1,
 		members: [
-			{ _id: 0, host : "mongodb0.example.net:27017" },
-			{ _id: 1, host : "mongodb1.example.net:27017" },
-			{ _id: 2, host : "mongodb2.example.net:27017" }
+			{ _id: 0, host : "ubuntu-mongo1:27017" },
+			{ _id: 1, host : "ubuntu-mongo2:27017" },
+			{ _id: 2, host : "ubuntu-mongo3:27017" }
 		]
 	}
 )
