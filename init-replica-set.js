9d581e7ec79208b11a4df21bc5c00c4137e65a16
Nikolai <gravcat@live.com>
2016-09-26 15:16:28 -0500

    Removed extraneous lines from rs.initiate method

--- init-replica-set.js
+++ init-replica-set.js
@@ -3,12 +3,9 @@ rs.initiate(
 		_id: "rs-savino",
 		version: 1,
 		members: [
-			{ _id: 0, host : "ubuntu-mongo1:27017" },
-			{ _id: 1, host : "ubuntu-mongo2:27017" },
-			{ _id: 2, host : "ubuntu-mongo3:27017" }
+			{ _id: 0, host : "ubuntu-mongo1:27017" }
 		]
 	}
 )
 rs.add('ubuntu-mongo2:27017');
 rs.add('ubuntu-mongo3:27017');
-rs.status();
